0e6896d36c7544da3af4203b1c3663091a51602b
ВХОД <vertep7@mail.ru>
2023-04-21 15:19:27 +0300

    4.1. Аргументы [#295521 #411351]

--- src/main/java/ru/job4j/ArgMethod.java
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -9,9 +9,9 @@ public class ArgMethod {
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
-        ArgMethod.hello(name,age);
-        ArgMethod.hello(name,age);
-        ArgMethod.hello(name,age);
-        ArgMethod.hello(name,age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
